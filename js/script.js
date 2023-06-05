c4a3cd396c59bcf66762d1373a8a589ec284dcf6
Flopiras <Flopiras@yahoo.it>
2023-06-05 15:09:39 +0200

    for version

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log('js ok')
+console.log('js ok');
+
+//1. Recuperare gli elementi dal DOM
+const list = document.getElementById('list');
+
+//2. Creare un collegamento per l'HTML
+let listItem = ``;
+
+//3. Preparare una lista con alcuni elementi all'interno
+const shoppingList = ['acqua', 'pane', 'zucchero', 'latte di unicorno', 'code di ragno' , 'alito di vento', 'lacrime di coccodrillo'];
+
+//4. Creare un ciclo che stampi gli elementi della lista **finchè** non arriva all'ultimo
+/* FOR VERSION
+for(let i = 0; i < shoppingList.length; i++){
+    listItem += `<li>${shoppingList[i]}</li>`
+}
+*/
+
+// ? WHILE VERSION
+
+//5. stampo in pagina
+list.innerHTML = listItem;
---
+++ readme.md
@@ -0,0 +1,14 @@
+**CONSEGNA:**
+
+Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+
+<hr>
+
+**TO DO**
+
+1. Recuperare gli elementi dal DOM
+2. Creare un collegamento per l'HTML
+3. Preparare una lista con alcuni elementi all'interno
+4. Creare un ciclo che stampi gli elementi della lista **finchè** non arriva all'ultimo
+   - individuare l'ultimo elemento della lista ed impostarlo come condizione finale del ciclo
+5. stampo in pagina
